512c84e9ba20e03f1950f8ad0afe4fe5be13d2cb
Enrico Dondero <enrico.dondero.p@gmail.com>
2023-11-17 12:29:06 +0100

    implemented GedBasicVectorWrtBase

--- MOCOM_LAB2_2023-24/MOCOM_LAB2/include/GetBasicVectorWrtBase.m
+++ MOCOM_LAB2_2023-24/MOCOM_LAB2/include/GetBasicVectorWrtBase.m
@@ -1,9 +1,13 @@
 function [r]=GetBasicVectorWrtBase(biTei, linkNumber)
-%%% GetBasicVectorWrtBase function 
+%%% GetBasicVectorWrtBase function
 % input :
-% biTei: trasnformation matrix in between frame i and frame j 
-% linkNumber: link number 
+% biTei: trasnformation matrix in between frame i and frame j
+% linkNumber: link number
 % output
 % r : basic vector from frame i to the robot base frame <0>
+r=[0;0;0];
+for i=1:linkNumber
+    r=r+biTei(1:3,4,i);
+end
 
 end
