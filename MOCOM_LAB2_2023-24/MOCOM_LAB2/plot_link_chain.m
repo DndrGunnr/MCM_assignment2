0fdb911d08e4b3c648d4f2004edf4982b1dfc71a
Enrico Dondero <enrico.dondero.p@gmail.com>
2023-11-27 15:09:20 +0100

    correctly implemented plot_link_chain and 1.5 exercise

--- MOCOM_LAB2_2023-24/MOCOM_LAB2/plot_link_chain.m
+++ MOCOM_LAB2_2023-24/MOCOM_LAB2/plot_link_chain.m
@@ -16,14 +16,27 @@ function plot_link_chain(qi,qf,iTj_q,linkType)
             
         end
     end
+    %bounds 
+    bounds_m=zeros(3,2);
+    for i=1:3
+        coords=joint_positions(i,:,:);
+        bounds_m(i,:)=[min(coords,[],'all')-1,max(coords,[],'all')+1];
+        if bounds_m(i,1)==bounds(i,2)
+            bounds_m(i,2)=bounds_m(i,2)+1;
+        end
+    end
+
     %init plot
     h=plot3(0,0,0,'-o','Color','b','MarkerSize',10,'MarkerFaceColor','#D9FFFF');
+    xlim(bounds_m(1,:));
+    ylim(bounds_m(2,:));
+    zlim(bounds_m(3,:));
     grid on;
     for i=1:numberOfSteps
         % Update line data
         set(h, 'XData', joint_positions(1,:,i), 'YData', joint_positions(2,:,i), 'ZData',joint_positions(3,:,i));
         % Reduce pause time for smoother animation
-        pause(0.01); 
+        pause(0.005); 
         % Refresh the plot
         drawnow;
     end
